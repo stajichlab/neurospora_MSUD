0bbab48eb8e25bcb564089e1a6c28368e9cbd675
Jason Stajich <jason@bioperl.org>
2014-12-01 21:23:00 -0800

    migrate the scripts for sRNA processing

--- scripts/sRNA_aln/cutadapt.sh
+++
@@ -1,3 +0,0 @@
-cutadapt -n 2 -a TCGTATGCCGTCTT -a TCGTATGCC -a GTTCAGAGTT -o ../cutadapt/PP.cutadapt.fq 101015_7589X1_s_6_sequence.txt 2> PP.cut.out &
-cutadapt -n 2 -a TCGTATGCCGTCTT -a TCGTATGCC -a GTTCAGAGTT -o ../cutadapt/2PF.cutadapt.fq 101015_7589X2_s_7_sequence.txt 2> 2PF.cut.out &
-cutadapt -n 2 -a TCGTATGCCGTCTT -a TCGTATGCC -a GTTCAGAGTT -o ../cutadapt/4PF.cutadapt.fq 101015_7589X3_s_8_sequence.txt 2> 4PF.cut.out &
--- scripts/sRNA_aln/fastx_clip.sh
+++
@@ -1,4 +0,0 @@
-zcat 101015_7589X1_s_6_sequence.txt.gz | fastx_clipper -a CTCGTATGCCGTCTTCTGCTTGT -n -l 17 | fastx_clipper -a CTCGTATGCCGT -n -o ../fastx_clip/PP.trim_3.fq &
-zcat 101015_7589X2_s_7_sequence.txt.gz | fastx_clipper -a CTCGTATGCCGTCTTCTGCTTGT -n -l 17 | fastx_clipper -a CTCGTATGCCGT -n -o ../fastx_clip/2PF.trim_3.fq &
-zcat 101015_7589X3_s_8_sequence.txt.gz | fastx_clipper -a CTCGTATGCCGTCTTCTGCTTGT -n -l 17 | fastx_clipper -a CTCGTATGCCGT -n -o ../fastx_clip/4PF.trim_3.fq &
-
