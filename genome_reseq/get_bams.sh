7a4c611be315dc8e68ba8f24a52b0488e4c24cbf
Jason Stajich <jason@bioperl.org>
2014-12-02 11:27:33 -0800

    change URL

--- genome_reseq/get_bams.sh
+++ genome_reseq/get_bams.sh
@@ -1,1 +1,1 @@
-curl -C - -O http://fungalgenomes.org/public/neurospora/data/support_files/wang_MSUD_2014/reseq/FGSC2489_reseq.merge.bam
+curl -C - -O http://fungalgenomes.org/public/neurospora/data/support_files/Wang_MSUD/reseq/FGSC2489_reseq.merge.bam
